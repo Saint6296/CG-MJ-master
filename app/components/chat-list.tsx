9d19a25af731decd62d334356fa77ed39411859e
Saint6296 <uisaint@gmail.com>
2023-07-20 22:42:12 +0800

    Update chat-list.tsx

--- app/components/chat-list.tsx
+++ app/components/chat-list.tsx
@@ -87,51 +87,6 @@ export function ChatItem(props: {
           </div>
         </div>
 
-        <div
-        className={`${styles["chat-item"]} ${
-          props.selected && styles["chat-item-selected"]
-        }`}
-        onClick={props.onClick}
-        ref={(ele) => {
-          draggableRef.current = ele;
-          provided.innerRef(ele);
-        }}
-        {...provided.draggableProps}
-        {...provided.dragHandleProps}
-        title={`${props.title}\n${Locale.ChatItem.ChatItemCount(
-          props.count,
-        )}`}
-        >
-        {props.narrow ? (
-          <div className={styles["chat-item-narrow"]}>
-            <div className={styles["chat-item-avatar"] + " no-dark"}>
-              <MaskAvatar mask={props.mask} />
-            </div>
-            <div className={styles["chat-item-narrow-count"]}>
-              {props.count}
-            </div>
-          </div>
-        ) : (
-          <>
-            <div className={styles["chat-item-title"]}>{props.title}</div>
-            <div className={styles["chat-item-info"]}>
-              <div className={styles["chat-item-count"]}>
-                {Locale.ChatItem.ChatItemCount(props.count)}
-              </div>
-              <div className={styles["chat-item-date"]}>
-                {new Date(props.time).toLocaleString()}
-              </div>
-            </div>
-          </>
-        )}
-
-        <div
-          className={styles["chat-item-delete"]}
-          onClickCapture={props.onDelete}
-        >
-          <DeleteIcon />
-        </div>
-        </div>
 
         
       )}
