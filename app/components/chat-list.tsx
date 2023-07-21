9899b8f1af3156e35d0613cd3b80823bb1099d87
Saint6296 <uisaint@gmail.com>
2023-07-21 18:25:18 +0800

    Update chat-list.tsx

--- app/components/chat-list.tsx
+++ app/components/chat-list.tsx
@@ -66,93 +66,105 @@ export function ChatItem(props: {
               </div>
             </div>
           ) : ( */}        
-              <><div className={styles["chat-item-title"]}>{props.title}</div><div className={styles["chat-item-info"]}>
-                <div className={styles["chat-item-count"]}>
-                  {Locale.ChatItem.ChatItemCount(props.count)}
-                </div>
-                <div className={styles["chat-item-date"]}>
-                  {new Date(props.time).toLocaleString()}
-                </div>
-              </div></>
+              <div className={styles["chat-item-title"]}>{props.title}</div><div className={styles["chat-item-info"]}>
+              <div className={styles["chat-item-count"]}>
+                {Locale.ChatItem.ChatItemCount(props.count)}
+              </div>
+              <div className={styles["chat-item-date"]}>
+                {new Date(props.time).toLocaleString()}
+              </div>
+            </div>
           {/* )} */}
 
-          <div
-            className={styles["chat-item-delete"]}
-            onClickCapture={props.onDelete}
-          >
+          <div className={styles["chat-item-delete"]} onClickCapture={props.onDelete}>
             <DeleteIcon />
           </div>
         </div>
-        
+  
+          <div className={`${styles["chat-item"]} ${props.selected && styles["chat-item-selected"]}`} onClick={props.onClick} title={`${props.title}\n${Locale.ChatItem.ChatItemCount(props.count,)}`}>
+            <div className={styles["chat-item-title"]}>{props.title}</div><div className={styles["chat-item-info"]}>
+              <div className={styles["chat-item-count"]}>
+                  {Locale.ChatItem.ChatItemCount(props.count)}
+              </div>
+              <div className={styles["chat-item-date"]}>
+                {new Date(props.time).toLocaleString()}
+              </div>
+            </div>
+
+          <div className={styles["chat-item-delete"]} onClickCapture={props.onDelete}>
+            <DeleteIcon />
+          </div>
+        </div>
+
         
     //   )}
     // </Draggable>
   );
 }
 
-export function ChatList(props: { narrow?: boolean }) {
-  const [sessions, selectedIndex, selectSession, moveSession] = useChatStore(
-    (state) => [
-      state.sessions,
-      state.currentSessionIndex,
-      state.selectSession,
-      state.moveSession,
-    ],
-  );
-  const chatStore = useChatStore();
-  const navigate = useNavigate();
+// export function ChatList(props: { narrow?: boolean }) {
+//   const [sessions, selectedIndex, selectSession, moveSession] = useChatStore(
+//     (state) => [
+//       state.sessions,
+//       state.currentSessionIndex,
+//       state.selectSession,
+//       state.moveSession,
+//     ],
+//   );
+//   const chatStore = useChatStore();
+//   const navigate = useNavigate();
 
-  const onDragEnd: OnDragEndResponder = (result) => {
-    const { destination, source } = result;
-    if (!destination) {
-      return;
-    }
+  // const onDragEnd: OnDragEndResponder = (result) => {
+  //   const { destination, source } = result;
+  //   if (!destination) {
+  //     return;
+  //   }
 
-    if (
-      destination.droppableId === source.droppableId &&
-      destination.index === source.index
-    ) {
-      return;
-    }
+  //   if (
+  //     destination.droppableId === source.droppableId &&
+  //     destination.index === source.index
+  //   ) {
+  //     return;
+  //   }
 
-    moveSession(source.index, destination.index);
-  };
+  //   moveSession(source.index, destination.index);
+  // };
 
-  return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="chat-list">
-        {(provided) => (
-          <div
-            className={styles["chat-list"]}
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-          >
-            {sessions.map((item, i) => (
-              <ChatItem
-                title={item.topic}
-                time={new Date(item.lastUpdate).toLocaleString()}
-                count={item.messages.length}
-                key={item.id}
-                id={item.id}
-                index={i}
-                selected={i === selectedIndex}
-                onClick={() => {
-                  navigate(Path.Chat);
-                  selectSession(i);
-                }}
-                onDelete={() => {
-                  if (!props.narrow || confirm(Locale.Home.DeleteChat)) {
-                    chatStore.deleteSession(i);
-                  }
-                }}
-                narrow={props.narrow}
-                mask={item.mask}
-              />
-            ))}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
-  );
+  // return (
+  //   <DragDropContext onDragEnd={onDragEnd}>
+  //     <Droppable droppableId="chat-list">
+  //       {(provided) => (
+  //         <div
+  //           className={styles["chat-list"]}
+  //           ref={provided.innerRef}
+  //           {...provided.droppableProps}
+  //         >
+  //           {sessions.map((item, i) => (
+  //             <ChatItem
+  //               title={item.topic}
+  //               time={new Date(item.lastUpdate).toLocaleString()}
+  //               count={item.messages.length}
+  //               key={item.id}
+  //               id={item.id}
+  //               index={i}
+  //               selected={i === selectedIndex}
+  //               onClick={() => {
+  //                 navigate(Path.Chat);
+  //                 selectSession(i);
+  //               }}
+  //               onDelete={() => {
+  //                 if (!props.narrow || confirm(Locale.Home.DeleteChat)) {
+  //                   chatStore.deleteSession(i);
+  //                 }
+  //               }}
+  //               narrow={props.narrow}
+  //               mask={item.mask}
+  //             />
+  //           ))}
+  //           {provided.placeholder}
+  //         </div>
+  //       )}
+  //     </Droppable>
+  //   </DragDropContext>
+  // );
 }
