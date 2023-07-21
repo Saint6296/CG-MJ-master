7cdbcb81252fb703fc22c788edc9e5d97780c81e
Saint6296 <uisaint@gmail.com>
2023-07-21 12:27:21 +0800

    Update chat-list.tsx

--- app/components/chat-list.tsx
+++ app/components/chat-list.tsx
@@ -30,35 +30,34 @@ export function ChatItem(props: {
   narrow?: boolean;
   mask: Mask;
 }) {
-        <div
-          className={`${styles["chat-item"]} ${
-            props.selected && styles["chat-item-selected"]
-          }`}
-          onClick={props.onClick}
-          title={`${props.title}\n${Locale.ChatItem.ChatItemCount(
-            props.count,
-          )}`}
-        >
-          {props.narrow ? (
-            <div className={styles["chat-item-narrow"]}>
-              <div className={styles["chat-item-avatar"] + " no-dark"}>
-                <MaskAvatar mask={props.mask} />
-              </div>
-              <div className={styles["chat-item-narrow-count"]}>
-                {props.count}
-              </div>
-            </div>
-          ) : (
+      <div
+        className={`${styles["chat-item"]} ${
+          props.selected && styles["chat-item-selected"]
+        }`}
+        onClick={props.onClick}
+        title={`${props.title}\n${Locale.ChatItem.ChatItemCount(
+          props.count,
+        )}`}
+      >
+      {props.narrow ? (
+        <div className={styles["chat-item-narrow"]}>
+          <div className={styles["chat-item-avatar"] + " no-dark"}>
+            <MaskAvatar mask={props.mask} />
+          </div>
+          <div className={styles["chat-item-narrow-count"]}>
+            {props.count}
+          </div>
+        </div>
+      ) : (
             
-              <><div className={styles["chat-item-title"]}>{props.title}</div><div className={styles["chat-item-info"]}>
-                <div className={styles["chat-item-count"]}>
-                  {Locale.ChatItem.ChatItemCount(props.count)}
-                </div>
-                <div className={styles["chat-item-date"]}>
-                  {new Date(props.time).toLocaleString()}
-                </div>
-              </div></>
-            </>
+            <><div className={styles["chat-item-title"]}>{props.title}</div><div className={styles["chat-item-info"]}>
+              <div className={styles["chat-item-count"]}>
+                {Locale.ChatItem.ChatItemCount(props.count)}
+              </div>
+              <div className={styles["chat-item-date"]}>
+                {new Date(props.time).toLocaleString()}
+              </div>
+            </div></>
           )}
 
           <div
@@ -68,7 +67,7 @@ export function ChatItem(props: {
             <DeleteIcon />
           </div>
         </div>
-}
+    }
 
 // export function ChatList(props: { narrow?: boolean }) {
 //   const [sessions, selectedIndex, selectSession, moveSession] = useChatStore(
